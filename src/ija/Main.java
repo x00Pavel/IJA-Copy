f054e8335f4d4f4dabe7a0e70dd80bca26eb9d7c
SnailRabbids <36141552+SnailRabbids@users.noreply.github.com>
2020-04-26 15:22:00 +0200

    reworked for dynamic create thread

--- src/ija/Main.java
+++ src/ija/Main.java
@@ -5,7 +5,6 @@ import ija.functional.Drawable;
 
 import ija.sample.BackEnd;
 import ija.sample.Updater;
-import ija.sample.ViewUpdater;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,7 +15,6 @@ import ija.sample.MainController;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
@@ -55,13 +53,6 @@ public class Main extends Application {
         list_bus = controller.buildLines(fileTransport);
         items.addAll(list_bus);
 
-        Thread b1 = new Thread(new BackEnd(list_bus.get(0)), "b1");
-        Thread b2 = new Thread(new BackEnd(list_bus.get(1)), "b2");
-        Thread b3 = new Thread(new BackEnd(list_bus.get(2)), "b3");
-        Thread updater = new Thread(new Updater(list_bus), "updater");
-//        Thread creator = new Thread(new ViewUpdater(primaryStage, scene, controller, items), "creator");
-
-//        executorService.submit(new ViewUpdater(primaryStage, scene, controller, items));
 
         controller.setElements(items);
         primaryStage.setScene(scene);
@@ -73,19 +64,15 @@ public class Main extends Application {
             e.printStackTrace();
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(4); // change 4->5 for ViewUpdater ON
-        executorService.submit(b1);
-        executorService.submit(b2);
-        executorService.submit(b3);
-//        executorService.submit(creator);
-//        controller.runUpdater();
-        executorService.submit(updater);
+        ExecutorService executorService = Executors.newFixedThreadPool(list_bus.size()+1); // change 4->5 for ViewUpdater ON
+        for (Bus actual_bus:list_bus) {
+            executorService.submit(new BackEnd(actual_bus));
+        }
+        executorService.submit(new Updater(list_bus));
 
         primaryStage.setOnCloseRequest(event -> {
             System.exit(0);
         });
-
-//        list_bus.get(0).Move();
 
     }
 }
