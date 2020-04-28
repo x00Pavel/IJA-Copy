b3abf6cd9de89bc0cadfe4ae024c211c4285ea31
x00Pavel <xyadlo00@stud.fit.vutb.cz>
2020-04-28 10:34:22 +0200

    Add dinamical Updater

--- src/ija/sample/Updater.java
+++ src/ija/sample/Updater.java
@@ -9,30 +9,23 @@ import java.util.List;
 
 public class Updater implements Runnable{
 
-    private List<Bus> bus_to_run;
+    private List<Bus> all_buses;
 
     public Updater(List<Bus> mybus){
-        this.bus_to_run = mybus;
+        this.all_buses = mybus;
     }
 
     @Override
     public void run() {
-
-        Circle bus_0 = (Circle)bus_to_run.get(0).getGUI().get(0);
-        Circle bus_1 = (Circle)bus_to_run.get(1).getGUI().get(0);
-        Circle bus_2 = (Circle)bus_to_run.get(2).getGUI().get(0);
         while(true) {
             Platform.runLater(new Runnable() {
                 @Override
                 public void run() {
-                    bus_0.setCenterX(bus_to_run.get(0).getBusX());
-                    bus_0.setCenterY(bus_to_run.get(0).getBusY());
-
-                    bus_1.setCenterX(bus_to_run.get(1).getBusX());
-                    bus_1.setCenterY(bus_to_run.get(1).getBusY());
-
-                    bus_2.setCenterX(bus_to_run.get(2).getBusX());
-                    bus_2.setCenterY(bus_to_run.get(2).getBusY());
+                    for(Bus bus: Updater.this.all_buses){
+                        Circle c = (Circle)bus.getGUI().get(0);
+                        c.setCenterX(bus.getBusX());
+                        c.setCenterY(bus.getBusY());
+                    }
                 }
             });
             try {
